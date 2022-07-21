ec0b623810a3f05a3ee82fad68674cf2bb71b104
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-21 15:18:17 +0530

    testing git

---
+++ week1/day_1.py
@@ -0,0 +1,78 @@
+# list1 = [["sujit", 1], ["larry", 2],
+# ["carry",3], ["marry","467"]]
+# dict1=dict(list1)
+# for items,lollypop in dict1.items():
+#     print(items ,lollypop)
+
+# for item ,lollypop in dict1.items():
+#  print(item ,"and looly is" , lollypop)
+# a= range(7)
+# print(a)
+# a=list(a)
+# print(a)
+# b=range(0,11,4)
+# b=list(b)
+# print(b)
+# for i in range(0,20,2):
+# #   print(i)
+# n = 5
+# for i in range(n):
+#     print(i * i)
+# def sujit(): 
+#     print("hello")
+#     print("hello world")
+
+# sujit()
+
+# for i in n:
+# leap=int(n)
+# n = 120
+# if (n % 4 == 0):
+#     if n/100 == 0:
+#         print("False")
+#         if n/400 == 0:
+#             print("True")
+#         else:
+#             print("False")
+#     else: 
+#         print("True")
+# else:
+#     print("False")
+
+    
+
+# leap=int(input())
+    
+# def function2(a,b):
+#     average=(a+b)/2
+#     print(average)
+#     return average
+
+# v= function2(4,5)   
+# print(v)
+# n=10
+# for i in range(1,n+1):
+# #  print(i, i*i, sep='@', end=' # ')
+# a= 200
+# b= 33
+# c= 500
+# if a > b:
+#     print("a is greater than b")
+# elif a == b:
+#     print("a is equal to b")
+    
+# else:
+#      print("b is greater than a")
+
+# print("a")if a > b else print("b")
+
+# if a > b or c > a :
+#     print("then say atleast one statement is true")
+
+# x=56
+# if x > 20:
+#     print("above 20")
+#     if x > 100:
+#         print("and also above 100")
+#     else:
+#        print("under 20") 
---
+++ week1/day_2.py
@@ -0,0 +1,8 @@
+# s=set()
+# print(type(s))
+# sujit=([1,2,3,4])
+# print(sujit)
+# s.add(1)
+# s.add(2)
+# s1 =s.intersection({1,2,3})
+# print(len(s))
