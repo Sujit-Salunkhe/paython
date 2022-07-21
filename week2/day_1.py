e0fdd606ec3f57c6207805cf77bdbd0d8b87b0d8
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-21 16:21:12 +0530

    learning classes in python

---
+++ week2/day_1.py
@@ -0,0 +1,17 @@
+# x = lambda a , b : a * b
+# print(x(5,6))
+
+# 
+# cars = ["Ford", "Volvo", "BMW"]
+# x=cars[2]
+# # print(x)
+# cars = ["Ford", "Volvo", "BMW"]
+# cars.append("honda")
+# print(cars)
+from re import X
+
+
+# class myclass:
+#     x=5
+# p1=myclass()
+# print(p1.x)
