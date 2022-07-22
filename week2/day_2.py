b673e8ea9293bbb652c5ef22f6f2b13903171587
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-22 23:32:36 +0530

    learning lists

---
+++ week2/day_2.py
@@ -0,0 +1,84 @@
+# class employees:
+#     pass
+# sujit=employees()
+# fujit=employees()
+# sujit.work="learning"
+# sujit.std="tybaf"
+# fujit.work="teaching"
+# x=5
+# y=10
+# print(x +y)
+# x=y=z="orange"
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+# x=5
+# print(type(x))
+# x = 1   
+# y = 2.8  
+# z = 1j   
+# #convert from int to float:
+# a = float(x)
+# #convert from float to int:
+# b = int(y)
+# #convert from int to complex:
+# c = complex(x)
+# # convert string from float to string
+# d=str(y)
+# 'hello'  "hello"
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+# a = "Hello, World!"
+# print(a[1])
+# for x in "banana":
+# #     print(x)
+# a="hello world"
+# print(len(a))
+# print(a[11])
+# a = "the beat car company in the world is lambo"
+# print("lambo" not in a)
+# if "lambo" in a:
+#     print("yes lambo is there")
+# else:
+# #     print("not there")
+# a="hello world"
+# print(a.upper())
+# 
+# z =   a.replace("h","d")
+# print(z)
+# # print(a.replace("h","d"))
+# a="hello, world"
+# b = a.split(",")
+# print(b)
+# a= "my name is sujit and age is {}"
+# age=20
+# print(a.format (age))
+# a= "my name is sujit and age is {}"
+# age=20
+# print(a.format (age))
+# tea = 10
+# biscuit = 5
+# medu_vada = 25
+# txt="the price of medu vada is {1} and the price of biscuit {2} and price of tea {0}"
+# print(txt.format(medu_vada,biscuit,tea))
+# print(len(txt))
+# # print(10<9)
+# print(bool("hello"))
+# print(bool(70<79))
+# x = "Hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+# print(type(a))
+# a=["s","f","h","uy","w"]
+# a[1:3]=["sujit" ,"sujit3"]
+# print(a)
+a=["s","f","h","uy","w"]
+a.insert(2,"sujit")
+print(a)
