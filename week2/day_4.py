90fe848328c741e65e52699aed3ae6b999f757a3
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-24 13:48:43 +0530

    learning tuples

---
+++ week2/day_4.py
@@ -0,0 +1,84 @@
+# import numbers
+# from re import A
+
+
+# print(len(numbers))
+# print(min(numbers))
+# print(max(numbers))
+# numbers.append(78)
+# numbers.pop(0)
+# numbers.remove(657)
+# del numbers[2]
+# for x in numbers:
+#     if 57 != x:
+#         print(x)
+# numbers[2]=5
+# numbers.insert(0,56)
+# b = numbers.replace(0,56)
+# print(numbers)
+# a=7
+# b=9
+# temp=a
+# a=b
+# b=temp
+# (a,b)=(b,a)
+# # print(a,b)
+# tuple =(56,67,78,90)
+# print(tuple [3])
+# print(len(tuple))
+# print(type(tuple))
+# numbers =(56,67,78,90)
+# y = list(numbers)
+# y.insert(2,78)
+# # y[2]=52
+# numbers=tuple(y)
+# print(numbers)
+# numbers =(56,67,78,90)
+# y =list(numbers)
+# y.append(90)
+# y.sort(reverse=True)
+# numbers=tuple(y)
+# print(numbers)
+# numbers =(56,67,78,90)
+# y=(67,)
+# numbers += y
+# print(numbers)
+# numbers =(56,67,78,90)
+# y =list(numbers)
+# y.append(59)
+# numbers=tuple(y)
+# print(numbers)
+# y=(56,)
+# numbers += y
+# print(numbers)
+# numbers=(56,89,89)
+# (green,yellow,fish)=numbers
+# print(green)
+# print(yellow)
+# print(fish)
+# (green,yellow,*fish)=numbers
+# print(green)
+# print(yellow)
+# print(fish)
+# numbers=(56,89,89,67,78,89)
+# for x in numbers:
+# #     print(x, sep='=',end='')
+# numbers=(56,89,89,67,78,89)
+# for x in range(len(numbers)):
+#     print(numbers[x])
+# tuple1 = ("a", "b" , "c")
+# tuple2 = (1, 2, 3)
+# x=list(tuple1)
+# y=list(tuple2)
+# x.append(y)
+# tuple1=x
+# tuple2=y
+# print(tuple1)
+# x=tuple1.append(tuple2)
+# # tuple3 = tuple1 + tuple2
+# # print(x)
+# tuple1 = ("a", "b" , "c")
+# x =tuple1.count(2)
+# print(x)
+def sujit():
+    pass
