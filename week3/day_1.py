bba74dcfad099b5d5fb4017e32217e858b9d31ac
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-25 17:35:50 +0530

    learning if else

--- week3/day_1.py
+++ week3/day_1.py
@@ -4,5 +4,29 @@
 # x=input()
 # y = sum(set(x))/(len(set(x)))
 # print y
-a="vm"
-print(isaln(a))
+# a="vm"
+# print(isaln(a))
+# a = 33
+# b = 200
+# if b > a:
+#   print("b is greater than a")
+#   a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+#   a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+#   a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
