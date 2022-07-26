588930b98ad96c03a6aa8c9a8255f85958cbeda1
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-26 17:43:58 +0530

    learning functions,loops

---
+++ week3/day_2.py
@@ -0,0 +1,104 @@
+from cgi import print_arguments
+from msilib.schema import Condition
+
+
+# # if else Condition
+# a=input()
+# b=input()
+# if a < b :
+#     print("second  is bigger")
+# elif a == b:
+#     print("the two numbers are equal")
+# else:
+#     print("first is bigger")
+# a=100
+# b=89
+# c=19
+# if c > 10 :
+#     print("c is greater than 10")
+#     if c > 20 :
+#         print("c is greater than 20")
+#     else:
+#         print("c is inbetween 10 and 20")
+# x=0
+# while x<40:
+#     print(x)
+#     x = x + 1
+# x=1
+# while x<6:
+#     # print(x)
+#     print(x)
+#     if x==3:
+#         break
+#     x += 1
+# x=0
+# while x<6:
+#     x+=1
+#     if x==3:
+#         continue
+#     print(x)
+# y=[2,1,21,78,96]
+# for i in y:
+#     print(i)
+# for x in "banana":
+#     print(x)
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     if x=="banana":
+#         continue
+#     print(x)
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+# for x in adj:
+#     for y in fruits:
+# #         print(x,y)
+# for x in range(6):
+#   if x == 3: break
+#   print(x)
+# else:
+#   print("Finally finished!")
+
+# #If the loop breaks, the e
+# for x in range(10):
+#     print(x)
+#     if x==3:
+#         break
+# else:
+#     print("finshed")
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     for y in adj:
+# #         print(x,y)
+# def sujit():
+#     print("hello sujit")
+# # sujit()
+# def sujit(x):
+#     print(x + " salunkhe" )
+    
+# sujit("vaishnav")
+# # sujit("sujit")
+# def sujit(first,last):
+#     print(first+" "+last)
+# sujit('sujit','salunkhe')
+# def functionsmy(*subjects):my_function
+#     print("l have subject"+" " +subjects[2])
+# # functionsmy("ma","fa","ca","cs")
+# def my_function(child1,child2,child3):
+#     print("l have 3 child's one of them is"+" "+child2)
+# my_function(child1="sujit",child2="vaishnav",child3="nahi hai")
+# def my_functions(**name):
+#     print("my last name is"+" "+name['fname'])
+# my_functions(lfname="salunkhe",fname="sujit")
+# def my_functions(country="india"):
+#     print("i am living in"+" "+country)
+# my_functions("america")
+# # my_functions()
+# def sujit(y):
+#     for x in y:
+#         print(x)
+# fruits = ["apple", "banana", "cherry"]
+# sujit("apple")
+def plus(x):
+   return  5 * x
+print(plus(5))
