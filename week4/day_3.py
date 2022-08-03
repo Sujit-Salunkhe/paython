eeafb301ff5b3817f2db5362c052de90962eb4c4
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-03 21:04:18 +0530

    learning inheritence of aclass

--- week4/day_3.py
+++ week4/day_3.py
@@ -19,10 +19,6 @@
 #         self.name=name
 #     def fuc(self):
 #         print("hello my name is "+ self.name)
-#         print("my age is " +  self.age)
-
-# sujit=marks("sujit","15")
-# # sujit.fuc()
 # class marks:
 #     def __init__(joke,name,std):
 #         joke.name=name
@@ -31,3 +27,58 @@
 #         print("my name is " + abd.name)
 # sujit=marks("sujit",90)
 # sujit.names()
+#         print("my age is " +  self.age)
+
+# a=person("sujit","salunkhe")
+# a.printname()
+# sujit=marks("sujit","15")
+# # sujit.fuc()
+# class marks:
+#         print("my name is " + abd.name)
+#     def __init__(joke,name,std):
+#         joke.name=name
+#         joke.std=std
+#     def names(abd):
+# sujit=marks("sujit",90)
+# sujit.names()
+class person:
+    def __init__(self,fname,lname):
+        self.firstname=fname
+        self.lname=lname
+        
+    def printname(self):
+        print(self.firstname,self.lname)
+    def std(self):
+        print("name is sujit" + self.firstname)
+class sujit(person):
+    def __init__(self, fname, lname,year):
+        super().__init__(fname, lname)
+        self.graduvation_year=year
+    def all(self):
+        print("welcome",self.firstname,self.lname,"graduvate in a year",self.graduvation_year)
+   
+x=sujit("sujit","salunkhe",2020)
+x.all()
+# x.printname()   
+
+
+
+"""
+creating a big data base for 100 millon users
+insert=the profile information for a new user.
+find=the profile information of a user given their usernames
+update=the profile information of  a user,given their username 
+list =all the users of the platform,sorted by username 
+
+
+"""
+
+class userdatabase:
+    def insert(self,user):
+        pass
+    def find(self,username):
+        pass
+    def update(self,user):
+        pass
+    def listall(self):
+        pass
