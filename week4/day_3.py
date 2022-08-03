de60ad32f281dc1adcb16198d899dcc94c3762c9
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-03 17:39:17 +0530

    learning classes and creating notes

---
+++ week4/day_3.py
@@ -0,0 +1,33 @@
+# class mycalss():
+#    x=5
+#    y=5
+# p=mycalss()
+# print(p.y)
+# class persons:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+# p1=persons("sujit",16)
+# print(p1.name)
+# print(p1.age)
+# sujit=marks()
+# sujit.work=15
+# print(sujit.__dict__    )
+# class marks:
+#     def __init__(self,name,age):
+#         self.age=age
+#         self.name=name
+#     def fuc(self):
+#         print("hello my name is "+ self.name)
+#         print("my age is " +  self.age)
+
+# sujit=marks("sujit","15")
+# # sujit.fuc()
+# class marks:
+#     def __init__(joke,name,std):
+#         joke.name=name
+#         joke.std=std
+#     def names(abd):
+#         print("my name is " + abd.name)
+# sujit=marks("sujit",90)
+# sujit.names()
