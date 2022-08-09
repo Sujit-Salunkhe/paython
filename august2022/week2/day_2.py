8fc4877052fa234702da3d48f27dcd27df7fc228
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-09 19:18:11 +0530

    writing a code about diagrams

---
+++ august2022/week2/day_2.py
@@ -0,0 +1,73 @@
+# from fnmatch import translate
+# from traceback import print_tb
+# txt="sujit is great person"
+# mytxt=txt.maketrans("s","v")
+# print(txt.translate(mytxt))
+# txt="sujit is great person"
+# print(txt.partition("great"))
+# txt="sujit is great person"
+# print(txt.replace("sujit","only sujit"))
+# txt="sujit is great person"
+# print(txt.rfind("sujit",8,10))
+# txt="sujit is great person"
+# print(txt.rindex("s"))
+# txt="sujit is great person"
+# print(txt.rsplit(",",1))
+# txt="sujit is great person".split()
+# print(txt)
+# txt="sujit is great person"
+# txt="sujit, is great ,person"
+# print(txt.split(","))
+# txt="sujit \nis great person"
+# print(txt)
+# print(txt.splitlines(True))
+# txt="sujit is great person"
+# # print(txt.startswith("sujit"))
+# from traceback import print_tb
+# txt="sujit is great person"
+# print(txt.strip(".,g"))
+# c=("sujit",)
+# print(type(c))
+# a="sujit"
+# list=[1,2,3,2,5,5]
+# list.append(4)
+# print(list)
+# txt="sujit is great person"
+# print(txt.swapcase())
+# txt="sujit is great person"
+# print(txt.title())
+# txt="sujit  is great person"
+# a="vais h n a v"
+# print(txt.translate(a))
+# txt="sujit is great person"
+# print(txt.upper())
+# txt="sujit is great person"
+# print(txt.capitalize())
+# txt="sujit is great person"
+# print(txt.lower())
+# txt=".10"
+# print(txt.zfill(100))
+# price=100
+# txt="the price of vada pav is {}".format(price)
+# # print(txt)
+# list=[1,2,3,45,5,45,5,5,54][1:6]
+# # print(list)
+# from xmlrpc.client import boolean
+
+
+# def digrams(a):
+#     b=input("select 1 or 0\n")
+#     p=1
+#     if  b=="1":
+#         while p<=a:
+#             print ("@"*p)
+#             p +=1
+#     else:
+#         while p<=a:
+#             print("@"*a)
+#             a -=1
+# a=digrams(int(input("how many lines you want\n")))
+
+
+    
+
