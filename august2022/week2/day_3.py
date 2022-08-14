6f493144c899977000bc25b596961b73412b98c9
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-14 19:12:25 +0530

    practising and learning and solving hackerrank

--- august2022/week2/day_3.py
+++ august2022/week2/day_3.py
@@ -35,14 +35,42 @@
 #         return 1
 #     else:
 #         return n * factorial(n-1)
-# print(factorial(int(input("enter a number"))))
-def fibonaci(n):
-    print("n:",n,"=","n-1=",(n-1),"n-2=",(n-2))
-    if n==1:
-        return 0
-    elif n==2:
-        return 1
-    else:
-        return fibonaci(n-1) + fibonaci(n-2)
-print(fibonaci(int(input("enter a number"))))
+# # print(factorial(int(input("enter a number"))))
+# def fibonaci(n):
+#     print("n:",n,"=","n-1=",(n-1),"n-2=",(n-2))
+#     if n==1:
+#         return 0
+#     elif n==2:
+#         return 1
+#     else:
+#         return fibonaci(n-1) + fibonaci(n-2)
+# print(fibonaci(int(input("enter a number"))))    
+from typing import Counter
+
+# # number_of_shoes=10
+# def function(a,b,c,)
+# shoes_sizes=2,3,4,5,6,7,8,9
+# custumers=1
+# desired_custemers,custumers={6:55,6:78}
+                        
+# list=[desired_custemers,custumers]
+# price=[]
+# i=0
+# while i < len(shoes_sizes):
+#       if shoes_sizes[i]==desired_custemers:
+#                 price.append(custumers)
+#                 i +=1
+
+# # print(list(map(lambda x y:x+y,price)))
+# s=[2,3,4,5,6,6,7,7]
+# b=[4,5,6,7,87]
+# # substract()
+# abc=49949464
+# print(abc)
+
+
+ 
+
+
+
 
---
+++ august2022/week2/day_4.py
@@ -0,0 +1,37 @@
+# b= "i am %s %s"%(a,c)
+# # print(b)
+# d="i am sujit {} {}"
+# b= d.format(a,c)
+# # print(b)
+# a="sujit"
+# c=45
+# d=f"i am sujit {a} {c} {45 * 3}"
+# print(d)
+# a=input()
+# b=input()
+# c=f"sujit goes to {a} for {b} dollers"
+# a=c.capitalize()
+# # print(a)
+# def function(*sujit):
+#     for i in sujit:
+#         print(i)
+# suj=[46,56,67,89,78,67,56,432,90]
+# # function(*suj)
+# for i in range(10):
+#     print("sujit is great man")
+# p=0
+# while p < 10:
+#     print("sujit is great man")
+# #     p += 1
+# l=[45,21,46,85,59]
+# for index,item in enumerate(l):
+#     # if index % 2 == 0:
+#         # print(item)
+#     print(index,item)
+from multiprocessing.sharedctypes import Value
+
+
+# dic={"sujit":"vadapav","fujit":"pizza"}
+# for key ,values in dic:
+#     print(key,values )
+
