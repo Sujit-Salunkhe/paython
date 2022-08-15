4d72f458c12b8b4b10f06276ec8c9b85ade56b09
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-15 19:27:40 +0530

    solving hackerrank

--- august2022/week3/day_1.py
+++ august2022/week3/day_1.py
@@ -37,9 +37,30 @@
 #     return excuter
 # @dec1
 # def sujit():
-#     print("devleper sujit")
-# sujit()
-a={1,2,3,4,5,6,7,8,9}
-b={45,56,45,48,45,5,1,2,3,5,7,8,88}
-a.intersection_update(b)
-print(a)    
+# #     print("devleper sujit")
+# # sujit()
+# a={1,2,3,4,5,6,7,8,9}
+# b={45,56,45,48,45,5,1,2,3,5,7,8,88}
+# a.intersection_update(b)
+# # print(a)  
+# s="sujit"  
+# a=s[0]
+# print(a)                
+# a=16
+# b={2,4,5,6,5,8,9,7,5,5,4,5,4,54,54,54,}     
+# c=4
+# d=intersection.update()
+# a=16
+# b=1 2 3 4 5 6 7 8 9 10 11 12 13 14 24 52
+# c=4
+# d=intersection_update 10
+# e=2 3 5 6 8 9 1 4 7 11
+# f=update 2
+# g=55 66
+# h=symmetric_difference_update 5
+# i=22 7 35 62 58
+# j=difference_update 7
+# k=11 22 35 55 58 62 66
+# # getattr 
+# sujit=(445,56,7,7,77,8,78,7,8)
+# print(hash(sujit))
