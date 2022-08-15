98737b675b8dd2af72ac897319b7265d08c8e52f
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-08-15 17:51:19 +0530

    learning decoraters in python

---
+++ august2022/week3/day_1.py
@@ -0,0 +1,45 @@
+# def fun():
+#     print("sujit is great")
+# fun2=fun
+# # del fun
+# # fun2()
+# def funnu(n):
+#     if n==0:
+#         return print
+#     if n==1:
+#         return sum
+
+# a=funnu(1)
+# print(a)
+# def printer(a):
+#     a("sum")
+# # a=printer(print)
+# # print(a)
+# def dec1(fun1):
+#     def excuter():
+#         print("excute")
+#         fun1()      
+#         print("excuted")
+#     return excuter
+# @dec1
+# def sujit():
+#     print("devleper sujit")
+# a=dec1(sujit)
+# # print(a)
+# a=dec1(sujit)
+# a()
+# sujit()
+# def dec1(fun1):
+#     def excuter():
+#         print("excute")
+#         fun1()      
+#         print("excuted")
+#     return excuter
+# @dec1
+# def sujit():
+#     print("devleper sujit")
+# sujit()
+a={1,2,3,4,5,6,7,8,9}
+b={45,56,45,48,45,5,1,2,3,5,7,8,88}
+a.intersection_update(b)
+print(a)    
