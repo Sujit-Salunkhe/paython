3d03aa46a224f75227474295fb40583050b83345
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-02 10:20:52 +0530

    solving a hacker rank

--- august2022/week4/day_4.py
+++ august2022/week4/day_4.py
@@ -15,39 +15,54 @@
 # print(amar.Email)
 
 
-from tkinter import CENTER
-from turtle import left
+from re import L
+# from tkinter import CENTER
+# from turtle import left
 
 
-def printstar(n):
-   p=1
-   while n//3 > p:
-      a="*"* p
-      print(a.center(n))
-      p +=2
+# def printstar(n):
+#    p=1
+#    while n//3 > p:
+#       a="*"* p
+#       print(a.center(n))
+#       p +=2
 
-   j=n//2 
-   c=n 
-   d=n
-   while j < c:
-      t="*" * d
-      print(t.center(n))
-      d -=4
-      c -=5
-   for x in range(3):
-      print(('*' * d ).center(n))
-   m=0
-   while m < d:
-      right=(("*" * ((d//2)-4)).just((n//2)-5))
-      left=(("*" * ((d//2)-4))).rjust((n//4)+4)
-      print(right,left)
-      d -=2
-      m +=1
+#    j=n//2 
+#    c=n 
+#    d=n
+#    while j < c:
+#       t="*" * d
+#       print(t.center(n))
+#       d -=4
+#       c -=5
+#    for x in range(3):
+#       print(('*' * d ).center(n))
+#    # print((("*" * (d+5))).center(n+1))
+#    m=0
+#    while m < d:
+#       left=(("*" * ((d//2)-m)))
+#       right=(("*" * ((d//2)-m))).rjust(n//2)
+#       b=left.ljust(n//2)
+#       print(b.rjust(n//2),right.ljust(n//2))
+#       d -=2
+#       m +=1
+      
+      
 
    
     
 
-printstar(50)        
-    
+# printstar(50)    
+# a=int(input("Type your first number\n"))
+# b=int(input("Type your second number\n"))
+# def plusminus(a,b):
+#    def plus(a,b): 
+#       return a +b 
+#    def mul(a,b) :
+#       return a * b
+#    print("Plus:",plus(a,b))    
+#    print("multiplication:",mul(a,b))    
+   
+# plusminus(a,b)
 
 
