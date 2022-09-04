c22562d7d59bde9d9e74e5379597f27dc08e6182
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-04 20:29:24 +0530

    solving questions of Hacker rank

---
+++ november2022/day_3.py
@@ -0,0 +1,36 @@
+import numbers
+from re import A
+
+
+# k=3
+# m=1000
+# t=list()
+# f=list()
+# for i in range(k):
+#         numbers=list(map(int,input().split()))
+#         numbers.pop(0)
+#         t.append(max(numbers))
+# def doubler(a):        
+#     for x in a:
+#         f.append(x * x)        
+#     return(sum(f)%1000)
+# # print(doubler(t))
+# k,m=list(map(int,input().split()))
+# t=list()
+# f=list()
+# for i in range(k):
+#         numbers=list(map(int,input().split()))
+#         numbers.pop(0)
+#         t.append(max(numbers))
+# def doubler(a):        
+#     for x in a:
+#         f.append(x * x)        
+#     return(sum(f)%m)
+# print(doubler(t))
+    
+
+
+
+
+    
+
--- november2022/week_1/day_1.py
+++ november2022/week_1/day_1.py
@@ -16,3 +16,19 @@
 #     print(i * pow(10,i)//9)
 # print(//9)
 # print(int(22))5
+# a={1,2,3,4,5,6,7}
+# b={1,2,3,4,5}
+# print(a.issuperset(b))
+# # print(x)
+# A = {1, 2, 3, 4, 5}
+# B = {1, 2, 3}
+# C = {1, 2, 3}
+
+# # Returns True
+# print(A.issuperset(B))
+
+# # Returns False
+# print(B.issuperset(A))
+
+# # Returns True
+# print(C.issuperset(B))
