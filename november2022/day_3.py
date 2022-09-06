923b9bb1d22fafa7ea7bd0ab3d19c92289757b40
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-09-06 17:08:12 +0530

    Solving Hacker rank questions

--- november2022/day_3.py
+++ november2022/day_3.py
@@ -1,3 +1,4 @@
+from cgi import print_directory
 import numbers
 from re import A
 
@@ -27,7 +28,63 @@ from re import A
 #         f.append(x * x)        
 #     return(sum(f)%m)
 # print(doubler(t))
-    
+a=1
+b=10
+c=100
+d=1
+# list=[a ,b,c,d]
+# min=min(list)
+# max=max(list)
+# print(max - min)
+# n=2
+# total=list()
+# for i in range(1,n+1):
+#     if (n % i ==0):
+#         total.append(i)
+
+# print(len(total))
+def catAndMouse(x, y, z):
+    if z < x and z < y:
+        a=x-z
+        b=y-z
+        if a > b:
+            return("Cat B")
+        elif a < b:
+            return("Cat B")
+        else:
+            return ("Mouse C")
+    if z > x and z > y:
+        a=z-y
+        b=z-x
+        if a > b:
+            return("Cat B")
+        elif a < b:
+            return("Cat B")
+        else:
+            return ("Mouse C")
+    if z > x and z < y:
+        a=z-x
+        b=y-z
+        if a > b:
+            return("Cat B")
+        elif a < b:
+            return("Cat B")
+        else:
+            return ("Mouse C")
+    if z < x and z > y:
+        a=x - z
+        b=z-y
+        if a > b:
+            return("Cat B")
+        elif a < b:
+            return("Cat B")
+        else:
+            return ("Mouse C")
+        
+   
+print(catAndMouse(1,2,3))
+print(catAndMouse(1,3,2))
+print(catAndMouse(5,78,100))
 
 
 
