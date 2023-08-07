fa6bb35dcfa161261eee182726e60470a2e87993
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-08-07 14:43:46 +0530

    updating my last fucntion

--- augest2023/week_2/day_1.py
+++ augest2023/week_2/day_1.py
@@ -12,18 +12,21 @@
 # print(t)
 # times=int(input())
 times = int(input())
+# for x in range(times):
+#     t=""
+#     s=""
+#     n=input()
+#     for i in range((len(n))):
+#         if(i%2 == 0):
+#             s +=n[i]
+#     for l in range((len(n))):
+#         if(l%2 != 0):
+#             t +=n[l]
+
+#     print(s,t)
 for x in range(times):
-    t=""
-    s=""
-    n=input()
-    for i in range((len(n))):
-        if(i%2 == 0):
-            s +=n[i]
-    for l in range((len(n))):
-        if(l%2 != 0):
-            t +=n[l]
-
-    print(s,t)
-
+    s=input()
+    print(s[::2],s[1::2])
+    
 
     
