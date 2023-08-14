a2606f7adf73b8114787d15abcd67c669dd412bf
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-08-14 17:54:02 +0530

    solve hackerrank

--- augest2023/week_2/day_4.py
+++ augest2023/week_2/day_4.py
@@ -37,7 +37,6 @@
 
 
 
-# print(pageCount(6,5))
 def pageCount(n, p):
     # Write your code here
     turn=0
@@ -47,7 +46,7 @@ def pageCount(n, p):
     if (n - p) >=  (n/2):
         turn=p//2
         return turn
-    if(n%2 ==0):
+    if(n%2==0):
         page=n
     else:
         page=n -1
@@ -56,5 +55,14 @@ def pageCount(n, p):
             return (turn)
         turn +=1
         page -=2
-
+print(pageCount(6,5))
+print(round(1//2))
+# fraction = 1/2
+# rounded_number = round(fraction)
+# print(rounded_number) 
 # print(3//2)
+# list=[23,89,45,34,21]
+# while(len(list)):
+#     list[]
+# s=1
+# s ? print(sujit):print("gamer")
---
+++ augest2023/week_2/day_6.py
@@ -0,0 +1,20 @@
+# b=5
+# k=[4]
+# p=[5]
+# bud=[]
+# for i in range(len(k)):
+#     for s in range(len(p)):
+#          if  (k[i] + p[s] <=b):
+#             bud.append(k[i] + p[s])
+# if (len(bud) == 0):
+#     print(-1)
+# else:
+#     print(max(bud))
+          
+
+if(-1):
+    print("sujit")
+
+s="sujit"
+for i in s:
+    print(i)
