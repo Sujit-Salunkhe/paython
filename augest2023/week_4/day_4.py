a4587f8400ceee96835e7b5f7c69904df0162e51
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-08-24 16:56:00 +0530

    laarning args map

--- augest2023/week_4/day_4.py
+++ augest2023/week_4/day_4.py
@@ -1,14 +1,20 @@
-s=[1,2,3,4,5,6,7,8,9]
-for x in range(int(input())):
-    t=input().split()
-    if t[0]=="pop":
-            if(len(s)):
-                s.pop()
-    elif t[0]=="remove" : 
-            a=int(t[1])
-            s.discard(a) 
-    elif t[0]=="discard": 
-            g=int(t[1])
-            s.discard(g)
+# s=[1,2,3,4,5,6,7,8,9]
+# for x in range(int(input())):
+#     t=input().split()
+#     if t[0]=="pop":
+#             if(len(s)):
+#                 s.pop()
+#     elif t[0]=="remove" : 
+#             a=int(t[1])
+#             s.discard(a) 
+#     elif t[0]=="discard": 
+#             g=int(t[1])
+#             s.discard(g)
         
-        
+s=[1,2,3,4,4,4,5,4,444]
+t=list(map(str,s))
+# print(t)
+# print(*s)
+s=[34,34]
+result =[x for x in s]
+print(result)
