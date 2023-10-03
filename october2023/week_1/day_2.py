1125ed17abaf706cab38cce8599bebe92913bd90
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-10-03 18:44:51 +0530

    solving hackerrangk

---
+++ october2023/week_1/day_2.py
@@ -0,0 +1,29 @@
+n=100 
+m=280 
+s=2 
+s=s-1
+# while n < m:
+#     m = m - n
+# pri=s+m
+
+# print(pri)
+# while m > 0:
+#     m-=1
+#     print("m",m)
+#     if (s==n):
+#         s=1
+#     else:
+#         s+=1
+#     print("s",s)
+
+
+# print(s)
+# s=s-1
+# while m > 0:
+#     m-=1
+#     if (s==n):
+#         s=1
+#     else:
+#         s+=1
+# print (s)
+
