4ecf2ad757f32bd62a3a904001dcc29a2b835809
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-10-13 19:15:20 +0530

    solving hackerank

--- october2023/week_2/day_2.py
+++ october2023/week_2/day_2.py
@@ -1,11 +1,19 @@
 n=25
-longnumber=n
-run=1
-for x in range(n):
-    run +=1
-    longnumber += longnumber * (n - 1)
-    print(longnumber)
-    n -= 1
-    print(n)
-print(longnumber)
+# longnumber=n
+# run=1
+# for x in range(n):
+#     run +=1
+#     longnumber += longnumber * (n - 1)
+#     print(longnumber)
+#     n -= 1
+#     print(n)
+# print(longnumber)
 # print(run)
+
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+fac=factorial(n)
+print(fac)
