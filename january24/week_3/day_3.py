d9b8620f7580fed2b0a4c81c38c87915b3e5497b
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-01-31 21:02:18 +0530

    solving

--- january24/week_3/day_3.py
+++ january24/week_3/day_3.py
@@ -10,5 +10,15 @@ print(str(b))
 print(repr(b))
 print(str(k))
 print(repr(k))
+s = [19,10,12,10,24,25,22]
+t =[]
+k= 4
+for i in range(len(s)):
+    for k in range(len(s)):
+        if (s[i] + s[k]) % k != 0:
+                t.append(s[i])
+                t.append(s[k])
+        else:
+             t.pop(s[k])
 
 
