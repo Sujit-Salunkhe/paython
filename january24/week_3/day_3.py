cad98b4504389af314c153563368b3fc3b3f1add
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-01-31 20:56:26 +0530

    solving hackerrank

---
+++ january24/week_3/day_3.py
@@ -0,0 +1,14 @@
+a = [1,2,3,4,5]
+b = 'Sample String'
+k ={
+    'name':'sujit',
+    "lname":'salunkhe'
+}
+print(str(a))
+print(repr(a))
+print(str(b))
+print(repr(b))
+print(str(k))
+print(repr(k))
+
+
